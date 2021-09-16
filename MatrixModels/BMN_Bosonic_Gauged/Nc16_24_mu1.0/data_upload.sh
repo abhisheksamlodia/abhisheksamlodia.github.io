f43e912a02359d9acb45b0786b96833ce0895377
abhisheksamlodia <66358220+abhisheksamlodia@users.noreply.github.com>
2021-09-16 15:18:24 +0530

    mu 1.0 N16 added

--- MatrixModels/BMN_Bosonic_Gauged/Nc16_24_mu1.0/data_upload.sh
+++ MatrixModels/BMN_Bosonic_Gauged/Nc16_24_mu1.0/data_upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-T=( '0.89' '0.891' '0.892' '0.893' '0.894' '0.895' '0.896' '0.897' '0.898' '0.899' '0.9' '0.901' '0.902' '0.903' '0.904' '0.905' )
+T=('0.895' '0.896' '0.897' '0.898' '0.899' '0.9' '0.901' '0.902' '0.903' '0.904' '0.905')
 for temp in "${T[@]}"
 do
 	rm -rf t$temp
@@ -9,8 +9,8 @@ do
 	cp ../Nc8_24_mu2/t0.8/index.html ./t$temp/
 	sed -ie "s/^<title>BMN_Bosonic_Gauged\/Nc8_24_mu2.0\/t0.8<\/title>.*/<title>BMN_Bosonic_Gauged\/Nc16_24_mu1.0\/t$temp<\/title>/" t$temp/index.html
 	sed -ie "s/^<h1>BMN_Bosonic_Gauged\/Nc8_24_mu2.0\/t0.8<\/h1>.*/<h1>BMN_Bosonic_Gauged\/Nc16_24_mu1.0\/t$temp<\/h1>/" t$temp/index.html
-	cp /Users/abhishek/Desktop/BMN_Results_Bosonic_paper/runs/1.0/L24N16/$temp/Output_Files/Results/*.png ./t$temp/Results/
+	cp /Users/abhishek/Documents/Coding_Projects/BMN_Matrix_Model/BMN_Bosonic/New_Analysis/1.0/L24N16/$temp/Output_Files/Results/*.png ./t$temp/Results/
 
 done
 
-cp /Users/abhishek/Desktop/BMN_Results_Bosonic_paper/runs/1.0/L24N16/Temperature_Plots/*.png ./Obs/Results/
+cp /Users/abhishek/Documents/Coding_Projects/BMN_Matrix_Model/BMN_Bosonic/New_Analysis/1.0/L24N16/*.png ./Obs/Results/
